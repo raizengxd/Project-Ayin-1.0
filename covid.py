aa20959d6ab9af5f990aef425f1b30aa4e8d151d
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-03-19 20:23:20 +0800

    trying to python only

--- covid.py
+++ covid.py
@@ -1,8 +1,3 @@
 
 import json,urllib.request
-url = "https://corona.lmao.ninja/countries"
-
-
-data = urllib.request.urlopen(url).read()
-output = json.loads(data)
-print (output)
+print(dir(urllib))
